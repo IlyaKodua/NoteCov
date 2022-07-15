5606c1222b2eb5e06204b470ae02ec5d34c4a29a
Ilya Kodua <ill.kodua@stc-spb.ru>
2022-07-15 20:35:29 +0300

    [+] add figs

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from utils import *
+import os
+import shutil
 
+
+if os.path.exists("data") and os.path.isdir("data"):
+    shutil.rmtree("data")
+
+os.mkdir("data")
 sr = 16000
 
-mat, freq = calc_corr_for_freqs(13, 12, sr)
+# mat, freq = calc_cov_for_freqs(13, 12, sr)
+# print(np.median(mat))
 
-print(np.median(mat))
-
-plot_and_save1(freq/440.0, mat)
+# plot_and_save1(freq/440.0, mat)
 
 
-mat, freq = calc_corr_for_freqs(1010, 1000, sr)
+mat1, freq = calc_corr_for_freqs_by_ebeling(1010, 1000, sr)
 
-plot_and_save2(freq/440.0, mat)
+mat1 -= np.median(mat1)
+mat1 /= np.max(mat1)
+# plot_and_save2(freq/440.0, mat, )
+
+
+mat2, freq = calc_cov_for_freqs(1010, 1000, sr)
+
+mat2 -= np.median(mat2)
+mat2 /= np.max(mat2)
+
+
+compare(mat1,mat2,freq/440.0)
+
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import find_peaks
-
+from scipy import signal
 
 def sig_div(f, duration, sr, alph):
     t = np.arange(0,duration, 1/sr)
@@ -9,9 +9,28 @@ def sig_div(f, duration, sr, alph):
     return np.real(sig)
 
 
+def sig_simple(f, duration, sr, alph):
+    t = np.arange(0,duration, 1/sr)
+    sig = np.sin(2 * np.pi * f * t) * np.exp(-alph * t) 
+    return sig
 
 
-def calc_corr_for_freqs(N, d, sr):
+
+
+def calc_corr_for_freqs_by_ebeling(N, d, sr):
+    mat = np.zeros(N+1)
+    freq = np.zeros(N+1)
+    for i in range(N+1):
+        freq[i] = freq_by_note(i/d)
+        x = sig_div(freq_by_note(0), 2, sr, 5)
+        y = sig_div(freq[i], 2, sr, 5)
+        mat[i] =  generalized_coincidence_function(x, y)
+    mat /= mat[0]
+    return mat, freq
+
+
+
+def calc_cov_for_freqs(N, d, sr):
     mat = np.zeros(N+1)
     freq = np.zeros(N+1)
     for i in range(N+1):
@@ -32,6 +51,26 @@ def correlation_time(x,y):
     return np.abs(np.sum(x * np.conj(y))) / np.sqrt( np.sum(np.abs(x)**2) * np.sum(np.abs(y)**2))
 
 
+# def get_spectrum(x):
+    
+#     spectrum = np.abs(np.fft.fft(x))
+#     spectrum = spectrum[0:int(len(spectrum)/2)]
+
+#     spectrum[1:len(spectrum)] *= 2
+#     spectrum /= len(x)
+#     return spectrum
+
+
+# def metric_of_harm(x):
+#     y = get_spectrum(x)
+#     plt.figure(1)
+#     plt.plot(y, 'r.')
+#     z = get_spectrum(y)
+#     plt.figure(2)
+#     plt.plot(z, 'b.')
+#     plt.show()
+#     return np.max(z) / np.sum(np.abs(z)**2)**(1/2)
+
 def plot_and_save1(x, y):
 
     plt.plot(x, y, 'b')
@@ -46,43 +85,51 @@ def plot_and_save1(x, y):
                 textcoords="offset points", # how to position the text
                 bbox=dict(boxstyle='round', fc='gray', alpha=0.1),
                 xytext=(0,10),
-                # fontsize = 7,
+                fontsize = 7,
                 ha='center') # horizontal alignment can be left, right or center
 
     plt.ylim([0.57, 1.05])
     plt.xlabel("Frequency ratio")
     plt.ylabel("Normalized covariance")
-    plt.savefig("figs/fig1.pdf", dpi=300, bbox_inches='tight')
+    plt.savefig("data/fig1.pdf", dpi=300, bbox_inches='tight')
 
     plt.show()
     
     pass
 
 
-def plot_and_save2(x, y):
+
+
+def plot_and_save2(x, y, name):
 
     plt.plot(x, y, 'b')
 
-    peaks, _ = find_peaks(y, distance=20)
-    plt.plot(x[peaks], y[peaks], 'rx')
-    for xs,ys in zip(x[peaks],y[peaks]):
 
-        labely = "{:.3f}".format(ys)
-        label = labely.replace('.',',')
-
-        if float(labely) >= 0.59:
-            plt.annotate(label, (xs,ys), # these are the coordinates to position the label
-                textcoords="offset points", # how to position the text
-                bbox=dict(boxstyle='round', fc='gray', alpha=0.1),
-                xytext=(0,10),
-                # fontsize = 7,
-                ha='center') # horizontal alignment can be left, right or cente
-
-    plt.ylim([0.57, 1.05])
+    # plt.ylim([0.57, 1.05])
     plt.xlabel("Frequency ratio")
-    plt.ylabel("Normalized covariance")
-    plt.savefig("figs/fig2.pdf", dpi=300, bbox_inches='tight')
+    plt.ylabel(name)
+    plt.savefig("data/fig2.pdf", dpi=300, bbox_inches='tight')
 
     plt.show()
     
     pass
+
+
+def  generalized_coincidence_function(sig1, sig2):
+    s = sig1 + sig2
+    corr = signal.correlate(s, s)
+    return np.sum(corr**2)
+
+
+def compare(x,y,freq):
+
+    plt.plot(freq, x, 'b',   label="GCF")
+    plt.plot(freq, y, 'r', label="Normalized covariance")
+    plt.legend(loc="upper right")
+    # plt.ylim([0.57, 1.05])
+    plt.xlabel("Frequency ratio")
+    # plt.ylabel(name)
+    plt.savefig("data/fig2.pdf", dpi=300, bbox_inches='tight')
+
+
+    plt.show()
